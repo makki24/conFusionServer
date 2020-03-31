b0108e6b4225107cd2e14b5fe28efb85e33a6f25
Syed Maqthyar <maqhti@gmail.com>
2020-03-31 12:29:35 +0530

    Basic Authenciation

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,6 +17,9 @@ connect.then((db)=>{
   console.log('connected correctly to the server');
 },(err)=>console.log(err));
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -27,6 +28,38 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+
+function auth(req,res,next)
+{
+  console.log(req.headers);
+
+  var authHeader =req.headers.authorization
+  if(!authHeader)
+  {
+    var err =new Error("You are not authenticated");
+    res.setHeader("WWW-Authenticate","Basic");
+    err.status=401;
+    next(err);
+    return;
+  }
+
+  var autha = new Buffer.from(authHeader.split(" ")[1],"base64").toString().split(":");
+  if(autha[0]=='admin' && autha[1]=='password')
+  {
+    next();
+  }
+  else
+  {
+    var err =new Error("You are not authenticated");
+    res.setHeader("WWW-Authenticate","Basic");
+    err.status =401;
+    next(err);
+    return;
+  }
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
