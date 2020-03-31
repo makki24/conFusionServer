021dba159258d1158acd7c578752a7013d6c7fca
Syed Maqthyar <maqhti@gmail.com>
2020-03-31 15:55:15 +0530

    Express Session part 1

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session =require('express-session');
+var FileStore= require('session-file-store')(session);
 
 var app = express();
 
@@ -27,35 +28,59 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
+//app.use(cookieParser('12345-67890-19876-54321'));
+app.use(session({
+    name:'session-id',
+    secret: '12345-67890-19876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store :new FileStore()
+}));
 
 function auth(req,res,next)
 {
-  console.log(req.headers);
+  console.log(req.session);
 
-  var authHeader =req.headers.authorization
-  if(!authHeader)
+  if(!req.session.user)
   {
-    var err =new Error("You are not authenticated");
-    res.setHeader("WWW-Authenticate","Basic");
-    err.status=401;
-    next(err);
-    return;
+        var authHeader =req.headers.authorization;
+        if(!authHeader)
+        {
+            var err =new Error("You are not authenticated");
+            res.setHeader("WWW-Authenticate","Basic");
+            err.status=401;
+            next(err);
+            return;
+        }
+
+        var autha = new Buffer.from(authHeader.split(" ")[1],"base64").toString().split(":");
+        if(autha[0]=='admin' && autha[1]=='password')
+        {
+            req.session.user ='admin';
+            next();
+        }
+        else
+        {
+            var err =new Error("You are not authenticated");
+            res.setHeader("WWW-Authenticate","Basic");
+            err.status =401;
+            next(err);
+            return;
+        }
   }
 
-  var autha = new Buffer.from(authHeader.split(" ")[1],"base64").toString().split(":");
-  if(autha[0]=='admin' && autha[1]=='password')
-  {
-    next();
-  }
   else
   {
-    var err =new Error("You are not authenticated");
-    res.setHeader("WWW-Authenticate","Basic");
-    err.status =401;
-    next(err);
-    return;
+       if(req.session.user==='admin')
+       {
+           next();
+       }
+       else
+       {
+          var err =new Error("You are not authenticated");
+          err.status =401;
+          next(err);
+       }
   }
 }
 
